a745cee33dc77e77cb8b2901a73b910f41f7b961
expola <40085912+expola@users.noreply.github.com>
2024-02-28 16:42:34 -0300

    help.js

--- commands/help.js
+++ commands/help.js
@@ -29,25 +29,25 @@ module.exports = {
     try {
       const musicCommandsEmbed = new EmbedBuilder()
         .setColor(client.config.embedColor)
-        .setTitle('🎸 **Music Commands**')
+        .setTitle('🎸 **Comandos Iniciais**')
         .addFields(
-          { name: '🎹 Play', value: 'Stream a song from a given link or text from sources' },
-          { name: '⏹️ Stop', value: 'Makes the bot stop playing music and leave the voice' },
-          { name: '📊 Queue', value: 'View and manage the song queue of this server' },
-          { name: '⏭️ Skip', value: 'Skip the current playing song' },
-          { name: '⏸️ Pause', value: 'Pause the currently playing song' },
-          { name: '▶️ Resume', value: 'Resume the current paused song' },
-          { name: '🔁 Loop', value: 'Toggle loop mode for queue and current song' },
-          { name: '🔄 Autoplay', value: 'Enable or disable autoplay [play random songs ]' },
-          { name: '⏩ Seek', value: 'Seek to a specific time in the current song' },
-          { name: '⏮️ Previous', value: 'Play the previous song in the queue' },
-          { name: '🔀 Shuffle', value: 'Shuffle the songs in queue' }
+          { name: '🎹 Play', value: 'Transmita uma música de um texto ex:[Mae do azzip] nao pode links' },
+          { name: '⏹️ Stop', value: 'Faz o bot parar de tocar música e sair do canal de voz' },
+          { name: '📊 Queue', value: 'Visualize e gerencie a fila de músicas deste servidor' },
+          { name: '⏭️ Skip', value: 'Ignorar a música que está sendo reproduzida no momento' },
+          { name: '⏸️ Pause', value: 'Pausar a música atualmente tocando' },
+          { name: '▶️ Resume', value: 'Retomar a música atualmente pausada' },
+          { name: '🔁 Loop', value: 'Alternar modo de loop para fila e música atual' },
+          { name: '🔄 Autoplay', value: 'Ative ou desative a reprodução automática [reproduzir músicas aleatórias]' },
+          { name: '⏩ Seek', value: 'Procure um horário específico na música atual' },
+          { name: '⏮️ Previous', value: 'Reproduzir a música anterior da fila' },
+          { name: '🔀 Shuffle', value: 'Embaralhe as músicas na fila' }
         )
         .setImage(`https://cdn.discordapp.com/attachments/1004341381784944703/1165201249331855380/RainbowLine.gif?ex=654f37ba&is=653cc2ba&hm=648a2e070fab36155f4171962e9c3bcef94857aca3987a181634837231500177&`); 
 
       const basicCommandsEmbed = new EmbedBuilder()
         .setColor(client.config.embedColor)
-        .setTitle('✨ **Basic Commands**')
+        .setTitle('✨ **Comandos Básicos**')
         .addFields(
           { name: '🏓 Ping', value: "Verifique a latência do bot" },
           { name: '🗑️ Clear', value: 'Limpe a fila de músicas deste servidor' },
@@ -59,21 +59,16 @@ module.exports = {
        .setImage('https://cdn.discordapp.com/attachments/1150827819547504741/1168917372267151370/standard.gif?ex=65538222&is=65410d22&hm=b4994392f44679da41fc9304eb69deaa3769e136057556deec0db69ae8d33a97&')
       const button1 = new ButtonBuilder()
         .setLabel('YouTube')
-        .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
+        .setURL('https://youtu.be/XyYJ1oXO64Q?list=RDMMXyYJ1oXO64Q')
         .setStyle(ButtonStyle.Link);
 
       const button2 = new ButtonBuilder()
         .setLabel('Discord')
-        .setURL('https://discord.gg/FUEHs7RCqz')
+        .setURL('https://discord.gg/7MFGDxX8Uk')
         .setStyle(ButtonStyle.Link);
-
-      const button3 = new ButtonBuilder()
-        .setLabel('Code')
-        .setURL('https://replit.com/@BEASTGAMERS1?tab=community')
-        .setStyle(ButtonStyle.Link);
-
+      
       const row = new ActionRowBuilder()
-        .addComponents(button1, button2, button3);
+        .addComponents(button1, button2);
 
       interaction.reply({
         embeds: [musicCommandsEmbed, basicCommandsEmbed],
