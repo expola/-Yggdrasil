5dd7542583a185f974fa9ce0193d9cf63d1b8caa
expola <40085912+expola@users.noreply.github.com>
2024-02-28 20:44:11 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -17,7 +17,7 @@ const { EmbedBuilder } = require('discord.js')
 const db = require("../mongoDB");
 module.exports = {
   name: "ping",
-  description: "check the bot latency",
+  description: "verifique a latência do bot",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
@@ -26,11 +26,11 @@ module.exports = {
     try {
 
       const start = Date.now();
-      interaction.reply("Pinging....").then(msg => {
+      interaction.reply("Pingando....").then(msg => {
         const end = Date.now();
         const embed = new EmbedBuilder()
           .setColor(`#6190ff`)
-          .setTitle(`Bot Latency`)
+          .setTitle(`Latência do bot`)
           .setDescription(`**Pong** : ${end - start}ms`)
         return interaction.editReply({ embeds: [embed] }).catch(e => { });
       }).catch(err => { })
