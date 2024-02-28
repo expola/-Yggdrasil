8796304ecc343a3989e1c209e57c9682c654cb47
expola <40085912+expola@users.noreply.github.com>
2024-02-28 20:16:51 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -21,7 +21,7 @@ const { ButtonStyle } = require('discord.js');
 
 module.exports = {
   name: "help",
-  description: "Get information about bot and commands.",
+  description: "Obtenha informações sobre bot e comandos.",
   permissions: "0x0000000000000800",
   options: [],
 
@@ -29,47 +29,47 @@ module.exports = {
     try {
       const musicCommandsEmbed = new EmbedBuilder()
         .setColor(client.config.embedColor)
-        .setTitle('🎸 **Music Commands**')
+        .setTitle('🎸 **Comandos musicais**')
         .addFields(
-          { name: '🎹 Play', value: 'Stream a song from a given link or text from sources' },
-          { name: '⏹️ Stop', value: 'Makes the bot stop playing music and leave the voice' },
-          { name: '📊 Queue', value: 'View and manage the song queue of this server' },
-          { name: '⏭️ Skip', value: 'Skip the current playing song' },
-          { name: '⏸️ Pause', value: 'Pause the currently playing song' },
-          { name: '▶️ Resume', value: 'Resume the current paused song' },
-          { name: '🔁 Loop', value: 'Toggle loop mode for queue and current song' },
-          { name: '🔄 Autoplay', value: 'Enable or disable autoplay [play random songs ]' },
-          { name: '⏩ Seek', value: 'Seek to a specific time in the current song' },
-          { name: '⏮️ Previous', value: 'Play the previous song in the queue' },
-          { name: '🔀 Shuffle', value: 'Shuffle the songs in queue' }
+          { name: '🎹 Play', value: 'Transmita uma música em forma de busca ex:[ Mae do azzip]' },
+          { name: '⏹️ Stop', value: 'Faz o bot parar de tocar música e sair da voz' },
+          { name: '📊 Queue', value: 'Visualize e gerencie a fila de músicas deste servidor' },
+          { name: '⏭️ Skip', value: 'Ignorar a música que está sendo reproduzida no momento' },
+          { name: '⏸️ Pause', value: 'Pausar a música atualmente tocando' },
+          { name: '▶️ Resume', value: 'Retomar a música atualmente pausada' },
+          { name: '🔁 Loop', value: 'Alternar modo de loop para fila e música atual' },
+          { name: '🔄 Autoplay', value: 'Ative ou desative a reprodução automática [reproduzir músicas aleatórias]' },
+          { name: '⏩ Seek', value: 'Procure um horário específico na música atual' },
+          { name: '⏮️ Previous', value: 'Reproduzir a música anterior da fila' },
+          { name: '🔀 Shuffle', value: 'Embaralhe as músicas na fila' }
         )
         .setImage(`https://cdn.discordapp.com/attachments/1004341381784944703/1165201249331855380/RainbowLine.gif?ex=654f37ba&is=653cc2ba&hm=648a2e070fab36155f4171962e9c3bcef94857aca3987a181634837231500177&`); 
 
       const basicCommandsEmbed = new EmbedBuilder()
         .setColor(client.config.embedColor)
-        .setTitle('✨ **Basic Commands**')
+        .setTitle('✨ **Comandos Básicos**')
         .addFields(
-          { name: '🏓 Ping', value: "Check the bot's latency" },
-          { name: '🗑️ Clear', value: 'Clear the song queue of this server' },
-          { name: '⏱️ Time', value: 'Display the current song playback time' },
-          { name: '🎧 Filter', value: 'Apply filters to enhance the sound as you love' },
-           { name: '🎵 Now Playing', value: 'Display the currently playing song information' },
-          { name: '🔊 Volume', value: 'Adjust the music volume [ hearing at high volumes is risky ]' },
+          { name: '🏓 Ping', value: "Verifique a latência do bot" },
+          { name: '🗑️ Clear', value: 'Limpe a fila de músicas deste servidor' },
+          { name: '⏱️ Time', value: 'Exibir o tempo de reprodução da música atual' },
+          { name: '🎧 Filter', value: 'Aplique filtros para aprimorar o som como você gosta' },
+           { name: '🎵 Now Playing', value: 'Exibir as informações da música atualmente sendo reproduzida' },
+          { name: '🔊 Volume', value: 'Ajuste o volume da música [ouvir em volumes altos é arriscado]' },
         ) 
        .setImage('https://cdn.discordapp.com/attachments/1150827819547504741/1168917372267151370/standard.gif?ex=65538222&is=65410d22&hm=b4994392f44679da41fc9304eb69deaa3769e136057556deec0db69ae8d33a97&')
       const button1 = new ButtonBuilder()
-        .setLabel('YouTube')
-        .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
+        .setLabel('Pedrada')
+        .setURL('https://youtu.be/XyYJ1oXO64Q?list=RDGMEM6ijAnFTG9nX1G-kbWBUCJAVMXyYJ1oXO64Q')
         .setStyle(ButtonStyle.Link);
 
       const button2 = new ButtonBuilder()
-        .setLabel('Discord')
-        .setURL('https://discord.gg/FUEHs7RCqz')
+        .setLabel('Arverez')
+        .setURL('https://discord.gg/4YGx2XAR')
         .setStyle(ButtonStyle.Link);
 
       const button3 = new ButtonBuilder()
-        .setLabel('Code')
-        .setURL('https://replit.com/@BEASTGAMERS1?tab=community')
+        .setLabel('Segredooo')
+        .setURL('https://youtu.be/Dat_r6NOoCM')
         .setStyle(ButtonStyle.Link);
 
       const row = new ActionRowBuilder()
